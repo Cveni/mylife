68eb695b371c6db0eccca9df33b3acf50df58f21
Szymon Zielonka <simon1991nam@gmail.com>
2014-11-03 21:59:54 +0100

    BaseManager - added methods for create activities, save location on activities and get activities location from database

--- src/com/mylife/BaseManager.java
+++ src/com/mylife/BaseManager.java
@@ -1,6 +1,8 @@
 package com.mylife;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.location.Location;
 
@@ -29,8 +31,67 @@ public class BaseManager
         baseHelper.close();
     }
 
+    public long createNewActivity()
+    {
+        ContentValues values = new ContentValues();
+        values.put(BaseHelper.ACTIVITIES_LOCATIONS, "");
+
+        open();
+        long id = database.insert(BaseHelper.TABLE_NAME_ACTIVITIES, null, values);
+        close();
+
+        return id;
+    }
+
     public void saveLocation(Location location, int activityIndex)
     {
-        
+        String whereClause = BaseHelper.ACTIVITIES_COLUMN_ID + " = ?";
+        String[] whereArgs = {String.valueOf(activityIndex)};
+
+        String[] cursorColumns = {BaseHelper.ACTIVITIES_LOCATIONS};
+        String[] cursorWhereArgs = {String.valueOf(activityIndex)};
+
+        open();
+
+        Cursor activityCursor = database.query(BaseHelper.TABLE_NAME_ACTIVITIES, cursorColumns,
+                BaseHelper.ACTIVITIES_COLUMN_ID + " = ?", cursorWhereArgs,
+                null, null, null, null);
+
+        if(activityCursor.moveToFirst())
+        {
+            String locations = activityCursor.getString(activityCursor.getColumnIndex(BaseHelper.ACTIVITIES_LOCATIONS))
+                    + ";" + location.getLatitude() + ":" + location.getLongitude();
+
+            ContentValues values = new ContentValues();
+            values.put(BaseHelper.ACTIVITIES_LOCATIONS, locations);
+
+            database.update(BaseHelper.TABLE_NAME_ACTIVITIES, values, whereClause, whereArgs);
+        }
+
+        activityCursor.close();
+        close();
+    }
+
+    public String getActivityLocations(int activityIndex)
+    {
+        String locations = null;
+        String[] cursorColumns = {BaseHelper.ACTIVITIES_LOCATIONS};
+        String[] cursorWhereArgs = {String.valueOf(activityIndex)};
+
+        open();
+
+        Cursor activityCursor = database.query(BaseHelper.TABLE_NAME_ACTIVITIES, cursorColumns,
+                BaseHelper.ACTIVITIES_COLUMN_ID + " = ?", cursorWhereArgs,
+                null, null, null, null);
+
+        if(activityCursor.moveToFirst())
+        {
+            locations = activityCursor.getString(activityCursor.getColumnIndex(BaseHelper.ACTIVITIES_LOCATIONS));
+        }
+
+        activityCursor.close();
+        close();
+
+        return locations;
     }
 }
