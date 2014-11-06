9764e7d7058c9de20349acbff2d83150ef7aaef9
Cveni <cveni7@gmail.com>
2014-11-06 15:50:20 +0100

    Some project structure changes

---
+++ app/src/main/java/mylife/org/mylife/BaseManager.java
@@ -0,0 +1,98 @@
+package mylife.org.mylife;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
+
+/**
+ * Created by Szymon on 2014-11-03.
+ */
+
+public class BaseManager
+{
+    private SQLiteDatabase database;
+    private BaseHelper baseHelper;
+
+    public BaseManager(Context context)
+    {
+        baseHelper = new BaseHelper(context);
+        database = null;
+    }
+
+    public void open()
+    {
+        if(database == null || !database.isOpen())
+            database = baseHelper.getWritableDatabase();
+    }
+
+    public void close()
+    {
+        baseHelper.close();
+    }
+
+    public long createNewActivity()
+    {
+        ContentValues values = new ContentValues();
+        values.put(BaseHelper.ACTIVITIES_LOCATIONS, "");
+
+        open();
+        long id = database.insert(BaseHelper.TABLE_NAME_ACTIVITIES, null, values);
+        close();
+
+        return id;
+    }
+
+    public void saveLocation(Location location, int activityIndex)
+    {
+        String whereClause = BaseHelper.ACTIVITIES_COLUMN_ID + " = ?";
+        String[] whereArgs = {String.valueOf(activityIndex)};
+
+        String[] cursorColumns = {BaseHelper.ACTIVITIES_LOCATIONS};
+        String[] cursorWhereArgs = {String.valueOf(activityIndex)};
+
+        open();
+
+        Cursor activityCursor = database.query(BaseHelper.TABLE_NAME_ACTIVITIES, cursorColumns,
+                BaseHelper.ACTIVITIES_COLUMN_ID + " = ?", cursorWhereArgs,
+                null, null, null, null);
+
+        if(activityCursor.moveToFirst())
+        {
+            String locations = activityCursor.getString(activityCursor.getColumnIndex(BaseHelper.ACTIVITIES_LOCATIONS))
+                    + ";" + location.getLatitude() + ":" + location.getLongitude();
+
+            ContentValues values = new ContentValues();
+            values.put(BaseHelper.ACTIVITIES_LOCATIONS, locations);
+
+            database.update(BaseHelper.TABLE_NAME_ACTIVITIES, values, whereClause, whereArgs);
+        }
+
+        activityCursor.close();
+        close();
+    }
+
+    public String getActivityLocations(int activityIndex)
+    {
+        String locations = null;
+        String[] cursorColumns = {BaseHelper.ACTIVITIES_LOCATIONS};
+        String[] cursorWhereArgs = {String.valueOf(activityIndex)};
+
+        open();
+
+        Cursor activityCursor = database.query(BaseHelper.TABLE_NAME_ACTIVITIES, cursorColumns,
+                BaseHelper.ACTIVITIES_COLUMN_ID + " = ?", cursorWhereArgs,
+                null, null, null, null);
+
+        if(activityCursor.moveToFirst())
+        {
+            locations = activityCursor.getString(activityCursor.getColumnIndex(BaseHelper.ACTIVITIES_LOCATIONS));
+        }
+
+        activityCursor.close();
+        close();
+
+        return locations;
+    }
+}
---
+++ app/src/main/java/mylife/org/mylife/StepCounter.java
@@ -0,0 +1,41 @@
+package mylife.org.mylife;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+/**
+ * Created by Szymon on 2014-11-03.
+ */
+
+public class StepCounter implements SensorEventListener
+{
+    private SensorManager sensorManager;
+    private double steps;
+
+    public void StepCounter(Context conext)
+    {
+        sensorManager = (SensorManager) conext.getSystemService(Context.SENSOR_SERVICE);
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(countSensor != null)
+            sensorManager.registerListener(this, countSensor, SensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent)
+    {
+        steps = sensorEvent.values[0];
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i)
+    {
+    }
+
+    public double getSteps()
+    {
+        return steps;
+    }
+}
