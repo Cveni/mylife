8c13455616fa430db7eb7878a63795d69c7ba025
Cveni <cveni7@gmail.com>
2015-01-15 10:52:04 +0100

    grand return of tha sport tabs

--- app/src/main/java/mylife/org/mylife/Sport.java
+++ app/src/main/java/mylife/org/mylife/Sport.java
@@ -3,135 +3,63 @@ package mylife.org.mylife;
 import java.util.ArrayList;
 import java.util.Locale;
 
-import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.Context;
-import android.graphics.Color;
 import android.location.Location;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.androidplot.xy.LineAndPointFormatter;
-import com.androidplot.xy.PointLabelFormatter;
 import com.androidplot.xy.SimpleXYSeries;
 import com.androidplot.xy.XYPlot;
 
-public class Sport extends Activity /*implements ActionBar.TabListener*/
+public class Sport extends FragmentActivity implements ActionBar.TabListener
 {
-    /**
-     * The {@link android.support.v4.view.PagerAdapter} that will provide
-     * fragments for each of the sections. We use a
-     * {@link FragmentPagerAdapter} derivative, which will keep every
-     * loaded fragment in memory. If this becomes too memory intensive, it
-     * may be best to switch to a
-     * {@link android.support.v13.app.FragmentStatePagerAdapter}.
-     */
-    //SectionsPagerAdapter mSectionsPagerAdapter;
-
-    /**
-     * The {@link ViewPager} that will host the section contents.
-     */
-    //ViewPager mViewPager;
+    ViewPager mViewPager;
+    SectionsPagerAdapter mSectionsPagerAdapter;
+    long id;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
+
+        id = getIntent().getLongExtra("id", -1);
+
         setContentView(R.layout.sport);
 
-        long id = getIntent().getLongExtra("id", -1);
-        //LinearLayout ll = (LinearLayout)findViewById(R.id.frag);
+        final ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        BaseManager bm = new BaseManager(getApplicationContext());
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
 
-        ArrayList<LocationModel> wynik = bm.getActivityLocations(id);
+        mViewPager = (ViewPager)findViewById(R.id.sportPager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        if(!wynik.isEmpty())
+        mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener()
         {
-            int n = wynik.size();
-            float[] result = new float[3];
-            float all = 0;
-
-            XYPlot plot = (XYPlot)findViewById(R.id.plot);
-            ArrayList<Double> xaxis = new ArrayList<Double>();
-            ArrayList<Double> yaxis = new ArrayList<Double>();
-
-            LocationModel last = wynik.get(0);
-            xaxis.add(0.0);
-            yaxis.add(0.0);
-
-            for (int i = 1; i < n; i++) {
-                LocationModel curr = wynik.get(i);
-
-                Location.distanceBetween(last.getLatitude(), last.getLongitude(), curr.getLatitude(), curr.getLongitude(), result);
-                all += result[0];
-
-                xaxis.add((double)all);
-                yaxis.add(((double)result[0]/(curr.getDateTimestamp()-last.getDateTimestamp()))*1000);
-
-                last = curr;
-            }
-
-            SimpleXYSeries series = new SimpleXYSeries(xaxis, yaxis, "Predkosc");
-            LineAndPointFormatter seriesFormat = new LineAndPointFormatter();
-            plot.addSeries(series, seriesFormat);
-
-            TextView tv = (TextView)findViewById(R.id.label1);
-            tv.setText("Wykonano " + n + " pomiarów");
-            TextView tv2 = (TextView)findViewById(R.id.label2);
-            tv2.setText("Pokonano " + Math.round(all) + " metrów");
-        }
-    }
-        // Set up the action bar.
-        //final ActionBar actionBar = getActionBar();
-        //actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        //mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        //mViewPager = (ViewPager) findViewById(R.id.pager);
-        //mViewPager.setAdapter(mSectionsPagerAdapter);
-
-        // When swiping between different sections, select the corresponding
-        // tab. We can also use ActionBar.Tab#select() to do this if we have
-        // a reference to the Tab.
-        /*mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
             @Override
-            public void onPageSelected(int position) {
+            public void onPageSelected(int position)
+            {
                 actionBar.setSelectedNavigationItem(position);
             }
-        });*/
+        });
 
-        // For each of the sections in the app, add a tab to the action bar.
-        //for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-            // Create a tab with text corresponding to the page title defined by
-            // the adapter. Also specify this Activity object, which implements
-            // the TabListener interface, as the callback (listener) for when
-            // this tab is selected.
-            /*actionBar.addTab(
-                    actionBar.newTab()
-                            .setText(mSectionsPagerAdapter.getPageTitle(i))
-                            .setTabListener(this));
+        for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++)
+        {
+            actionBar.addTab(actionBar.newTab().setText(mSectionsPagerAdapter.getPageTitle(i)).setTabListener(this));
         }
-    }*/
+    }
 
-
-   /* @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.sport_menu, menu);
@@ -151,19 +79,12 @@ public class Sport extends Activity /*implements ActionBar.TabListener*/
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 
     @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        // When the given tab is selected, switch to the corresponding page in
-        // the ViewPager.
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction)
+    {
         mViewPager.setCurrentItem(tab.getPosition());
-        long id = getIntent().getLongExtra("id", -1);
-        LinearLayout ll = (LinearLayout)findViewById(R.id.frag);
-
-        TextView tv = new TextView(getApplicationContext());
-        tv.setText("bumbum"+id);
-        ll.addView(tv);
     }
 
     @Override
@@ -172,78 +93,193 @@ public class Sport extends Activity /*implements ActionBar.TabListener*/
 
     @Override
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }*/
+    }
 
-    /**
-     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-     * one of the sections/tabs/pages.
-     */
-    /*public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-        public SectionsPagerAdapter(FragmentManager fm) {
+    public class SectionsPagerAdapter extends FragmentPagerAdapter
+    {
+        public SectionsPagerAdapter(FragmentManager fm)
+        {
             super(fm);
         }
 
         @Override
-        public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+        public Fragment getItem(int position)
+        {
+            return SportPage.newInstance(id, position);
         }
 
         @Override
-        public int getCount() {
-            // Show 3 total pages.
-            return 3;
+        public int getCount()
+        {
+            return 6;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             Locale l = Locale.getDefault();
-            switch (position) {
+            switch (position)
+            {
                 case 0:
                     return getString(R.string.sport_tab1);
                 case 1:
                     return getString(R.string.sport_tab2);
                 case 2:
                     return getString(R.string.sport_tab3);
+                case 3:
+                    return getString(R.string.sport_tab4);
+                case 4:
+                    return getString(R.string.sport_tab5);
+                case 5:
+                    return getString(R.string.sport_tab6);
             }
             return null;
         }
-    }*/
+    }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    //public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-      //  private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-    /*    public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
+    public static class SportPage extends Fragment
+    {
+        public static SportPage newInstance(long id, int page)
+        {
+            SportPage fragment = new SportPage();
             Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            args.putLong("id", id);
+            args.putInt("page", page);
             fragment.setArguments(args);
+
             return fragment;
         }
 
-        public PlaceholderFragment() {
-        }
+        public SportPage() {}
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.sport_fragment, container, false);
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            int page = getArguments().getInt("page");
 
-            return rootView;
+            switch (page)
+            {
+                case 0:
+                    return page1(inflater, container, savedInstanceState);
+                case 1:
+                    return page2(inflater, container, savedInstanceState);
+                case 2:
+                    return page3(inflater, container, savedInstanceState);
+                case 3:
+                    return page4(inflater, container, savedInstanceState);
+                case 4:
+                    return page5(inflater, container, savedInstanceState);
+                case 5:
+                    return page6(inflater, container, savedInstanceState);
+            }
+
+            return null;
         }
-    }*/
 
+        public View page1(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View page = inflater.inflate(R.layout.sport_page1, container, false);
+
+            return page;
+        }
+
+        public View page2(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View page = inflater.inflate(R.layout.sport_page2, container, false);
+
+            return page;
+        }
+
+        public View page3(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View page = inflater.inflate(R.layout.sport_page3, container, false);
+
+            BaseManager bm = new BaseManager(getActivity().getApplicationContext());
+
+            ArrayList<LocationModel> wynik = bm.getActivityLocations(getArguments().getLong("id"));
+
+            if(!wynik.isEmpty()) {
+                int n = wynik.size();
+                float[] result = new float[3];
+                float all = 0;
+
+                XYPlot plot = (XYPlot) page.findViewById(R.id.gpsPlot);
+                ArrayList<Double> xaxis = new ArrayList<Double>();
+                ArrayList<Double> yaxis = new ArrayList<Double>();
+
+                LocationModel last = wynik.get(0);
+                xaxis.add(0.0);
+                yaxis.add(0.0);
+
+                for (int i = 1; i < n; i++) {
+                    LocationModel curr = wynik.get(i);
+
+                    Location.distanceBetween(last.getLatitude(), last.getLongitude(), curr.getLatitude(), curr.getLongitude(), result);
+                    all += result[0];
+
+                    xaxis.add((double) all);
+                    yaxis.add(((double) result[0] / (curr.getDateTimestamp() - last.getDateTimestamp())) * 1000);
+
+                    last = curr;
+                }
+
+                SimpleXYSeries series = new SimpleXYSeries(xaxis, yaxis, "Predkosc");
+                LineAndPointFormatter seriesFormat = new LineAndPointFormatter();
+                plot.addSeries(series, seriesFormat);
+            }
+
+            return page;
+        }
+
+        public View page4(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View page = inflater.inflate(R.layout.sport_page4, container, false);
+
+            return page;
+        }
+
+        public View page5(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View page = inflater.inflate(R.layout.sport_page5, container, false);
+
+            BaseManager bm = new BaseManager(getActivity().getApplicationContext());
+
+            ArrayList<LocationModel> wynik = bm.getActivityLocations(getArguments().getLong("id"));
+
+            if(!wynik.isEmpty()) {
+                int n = wynik.size();
+                float[] result = new float[3];
+                float all = 0;
+
+                ArrayList<Double> xaxis = new ArrayList<Double>();
+                ArrayList<Double> yaxis = new ArrayList<Double>();
+
+                LocationModel last = wynik.get(0);
+                xaxis.add(0.0);
+                yaxis.add(0.0);
+
+                for (int i = 1; i < n; i++) {
+                    LocationModel curr = wynik.get(i);
+
+                    Location.distanceBetween(last.getLatitude(), last.getLongitude(), curr.getLatitude(), curr.getLongitude(), result);
+                    all += result[0];
+
+                    last = curr;
+                }
+
+                TextView tv = (TextView) page.findViewById(R.id.label1);
+                tv.setText("Wykonano " + n + " pomiarów");
+                TextView tv2 = (TextView) page.findViewById(R.id.label2);
+                tv2.setText("Pokonano " + Math.round(all) + " metrów");
+            }
+
+            return page;
+        }
+
+        public View page6(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View page = inflater.inflate(R.layout.sport_page6, container, false);
+
+            return page;
+        }
+    }
 }
