2723672a2f676fedb39cd6967989f570982deaf7
Cveni <cveni7@gmail.com>
2015-01-18 12:05:35 +0100

    added checking if gps is on

--- app/src/main/java/mylife/org/mylife/Main.java
+++ app/src/main/java/mylife/org/mylife/Main.java
@@ -1,9 +1,15 @@
 package mylife.org.mylife;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.LocationManager;
 import android.os.Bundle;
 //import android.view.Menu;
+import android.preference.PreferenceManager;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
@@ -52,6 +58,8 @@ public class Main extends Activity
 
     public void btn1(View v)
     {
+        if(!checkGPS()) alertGPS();
+
         //Intent i = new Intent(getApplicationContext(), Sport.class);
         //startActivity(i);
     }
@@ -85,5 +93,38 @@ public class Main extends Activity
     public void akcja3(View v)
     {
         Toast.makeText(getApplicationContext(), "Pokonano "+sc.getSteps()+" kroków", Toast.LENGTH_LONG).show();
+    }
+
+    public boolean checkGPS()
+    {
+        SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        boolean gps = settings.getBoolean(getResources().getString(R.string.settings_gps_use_key), true);
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+
+        if(gps && !locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) return false;
+        else return true;
+    }
+
+    public void alertGPS()
+    {
+        AlertDialog alert = new AlertDialog.Builder(this).create();
+        alert.setTitle(getResources().getString(R.string.main_alert_gps_title));
+        alert.setMessage(getResources().getString(R.string.main_alert_gps_msg));
+        alert.setButton(DialogInterface.BUTTON_NEGATIVE, getResources().getString(R.string.alert_negative), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        alert.setButton(DialogInterface.BUTTON_POSITIVE, getResources().getString(R.string.alert_settings), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which)
+            {
+                Intent i = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
+            }
+        });
+
+        alert.show();
     }
 }
