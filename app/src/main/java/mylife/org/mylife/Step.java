e8d2134a3d9810f3cc231742fb1add88674b86f3
Cveni <cveni7@gmail.com>
2015-01-28 15:06:42 +0100

    added missing step stats

--- app/src/main/java/mylife/org/mylife/Step.java
+++ app/src/main/java/mylife/org/mylife/Step.java
@@ -4,8 +4,12 @@ import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v13.app.FragmentStatePagerAdapter;
 import android.support.v4.app.FragmentActivity;
@@ -15,6 +19,10 @@ import android.view.LayoutInflater;
 //import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.GridView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.androidplot.xy.LineAndPointFormatter;
 import com.androidplot.xy.SimpleXYSeries;
@@ -169,7 +177,62 @@ public class Step extends FragmentActivity implements ActionBar.TabListener
 
         public View page1(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            View page = inflater.inflate(R.layout.step_page1, container, false);
+            final View page = inflater.inflate(R.layout.step_page1, container, false);
+
+            final ArrayList<GridItem> gi = new ArrayList<GridItem>();
+            String[] titles = getResources().getStringArray(R.array.step_info_titles);
+            String[] data = {"", "", ""};
+
+            TextView tvDate = (TextView)page.findViewById(R.id.step_date_value);
+            tvDate.setText(getActivity().getIntent().getStringExtra("name"));
+            TextView tvSpace = (TextView)page.findViewById(R.id.step_date_space);
+            tvSpace.setTextSize((float)(tvSpace.getTextSize()/GridAdapter.spaceScale));
+
+            BaseManager bm = new BaseManager(getActivity().getApplicationContext());
+
+            Calendar day = Calendar.getInstance();
+            day.setTimeInMillis(getArguments().getLong("date"));
+            ArrayList<StepModel> steps = bm.getStepsByDay(day);
+
+            int stepSum = getStepSum(steps);
+
+            data[0] = stepSum+"";
+            data[1] = (double)(Math.round(10*((double)stepSum)/24)) / 10 + " "+getResources().getString(R.string.unit_per_hour);
+            data[2] = stepsCalories(stepSum)+" "+getResources().getString(R.string.unit_kcal);
+
+            for(int i = 0; i < 3; i++)
+            {
+                gi.add(new GridItem(titles[i], data[i], false));
+            }
+
+            ViewTreeObserver vto = page.getViewTreeObserver();
+            vto.addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener()
+            {
+                @Override
+                public void onGlobalLayout()
+                {
+                    RelativeLayout cell = (RelativeLayout)page.findViewById(R.id.step_date_cell);
+                    GridAdapter ga = new GridAdapter(getActivity(), R.layout.grid_item, gi);
+                    GridView gv = (GridView)page.findViewById(R.id.step_info_grid);
+
+                    if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT)
+                    {
+                        cell.setMinimumHeight(page.getMeasuredHeight()/4);
+                        ga.setCellHeight(page.getMeasuredHeight()/4);
+                        gv.setNumColumns(2);
+                    }
+                    else if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
+                    {
+                        cell.setMinimumHeight(page.getMeasuredHeight()/2);
+                        ga.setCellHeight(page.getMeasuredHeight()/2);
+                        gv.setNumColumns(4);
+                    }
+
+                    gv.setAdapter(ga);
+
+                    page.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                }
+            });
 
             return page;
         }
@@ -205,7 +268,7 @@ public class Step extends FragmentActivity implements ActionBar.TabListener
             return seriesFormat;
         }
 
-        public ArrayList<Integer> getStepIntervals(ArrayList<StepModel> steps)
+        public static ArrayList<Integer> getStepValues(ArrayList<StepModel> steps)
         {
             ArrayList<Integer> intervals = new ArrayList<Integer>();
 
@@ -213,7 +276,9 @@ public class Step extends FragmentActivity implements ActionBar.TabListener
             int last = 0;
             intervals.add(0);
 
-            for(int i = 1; i < steps.size(); i++)
+            int n = steps.size();
+
+            for(int i = 1; i < n; i++)
             {
                 int curr = (int)steps.get(i).getValue();
 
@@ -230,9 +295,42 @@ public class Step extends FragmentActivity implements ActionBar.TabListener
             return intervals;
         }
 
-        public SimpleXYSeries getStepSeries(ArrayList<StepModel> steps)
+        public static ArrayList<Integer> getStepIntervals(ArrayList<StepModel> steps)
+        {
+            ArrayList<Integer> values = getStepValues(steps);
+
+            ArrayList<Integer> intervals = new ArrayList<Integer>();
+            intervals.add(0);
+
+            int n = steps.size();
+
+            for(int i = 1; i < n; i++)
+            {
+                intervals.add(values.get(i)-values.get(i-1));
+            }
+
+            return intervals;
+        }
+
+        public static int getStepSum(ArrayList<StepModel> steps)
         {
             ArrayList<Integer> intervals = getStepIntervals(steps);
+
+            int sum = 0;
+
+            int n = steps.size();
+
+            for(int i = 0; i < n; i++)
+            {
+                sum += intervals.get(i);
+            }
+
+            return sum;
+        }
+
+        public static SimpleXYSeries getStepSeries(ArrayList<StepModel> steps)
+        {
+            ArrayList<Integer> values = getStepValues(steps);
 
             ArrayList<Double> xAxis = new ArrayList<Double>();
             ArrayList<Double> yAxis = new ArrayList<Double>();
@@ -246,10 +344,49 @@ public class Step extends FragmentActivity implements ActionBar.TabListener
                 Calendar time = curr.getDate();
                 xAxis.add(time.get(Calendar.HOUR_OF_DAY)+(double)time.get(Calendar.MINUTE)/60.0);
 
-                yAxis.add((double)(intervals.get(i))/1000);
+                yAxis.add((double)(values.get(i))/1000);
             }
 
             return new SimpleXYSeries(xAxis, yAxis, null);
+        }
+
+        public int stepsCalories(int dayCount)
+        {
+            SharedPreferences settings = PreferenceManager.getDefaultSharedPreferences(getActivity().getApplicationContext());
+            Resources res = getResources();
+
+            boolean isMale = settings.getString(res.getString(R.string.settings_user_gender_key), "M").equals("M");
+            double weight = Integer.parseInt(settings.getString(res.getString(R.string.settings_user_weight_key), "80"));
+            double height = Integer.parseInt(settings.getString(res.getString(R.string.settings_user_height_key), "180"));
+            double age = Integer.parseInt(settings.getString(res.getString(R.string.settings_user_age_key), "25"));
+            String intense = settings.getString(res.getString(R.string.settings_user_acti_key), "S");
+
+            double burned = dayCount * 0.045;
+            double brm;
+
+            if(isMale)
+            {
+                brm = (9.99*weight) + (6.25*height) - (4.92*age) + 5;
+            }
+            else
+            {
+                brm = (9.99*weight) + (6.25*height) - (4.92*age) - 161;
+            }
+
+            if(intense.equals("M"))
+            {
+                brm += 200;
+            }
+            else if(intense.equals("S"))
+            {
+                brm += 400;
+            }
+            else
+            {
+                brm += 600;
+            }
+
+            return (int) (brm+burned);
         }
     }
 }
